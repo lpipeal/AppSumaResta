43096e8fee603417d8e8682822855f905ab15ba3
felipe <lpipeal@gmail.com>
2019-12-17 13:22:25 -0500

    App Design

---
+++ app/src/main/java/co/dinosoft/appsumaresta/MainActivity.java
@@ -0,0 +1,14 @@
+package co.dinosoft.appsumaresta;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
